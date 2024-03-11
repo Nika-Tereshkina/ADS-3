44f7f76fad23e5072f785bbf7252727c3eacafa5
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-11 21:51:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+    int counter = 0;
+    int left = 0;
+    int right = size - 1;
+    
+    while (left <= right) {
+        int average = left + (right - left) / 2;
+        if (arr[average] == value) {
+            counter++;
+        int i = average - 1;
+            while (i >= 0 && arr[i] == value) {
+                counter++;
+                i--;
+            }
+            int j = average + 1;
+            while (j < size && arr[j] == value) {
+                counter++;
+                j++;
+            }
+            return counter;
+            } else if (arr[average] < value) {
+            left = average + 1;
+            } else {
+            right = average - 1;
+        }
+    }
   return 0; // если ничего не найдено
 }
