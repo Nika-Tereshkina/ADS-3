370af9331dc87927c25b3625af8b1b9a07dd6d18
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-11 22:09:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,6 @@ int cbinsearch(int *arr, int size, int value) {
     int counter = 0;
     int left = 0;
     int right = size - 1;
-    
     while (left <= right) {
         int average = left + (right - left) / 2;
         if (arr[average] == value) {
